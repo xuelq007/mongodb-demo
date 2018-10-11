0f36742ad0f3bcf042be3de6093a2c9cdd3ecf23
xuelq007 <xueliqiang1988@sina.com>
2018-10-11 13:10:19 +0800

    后台管理api

--- demo.js
+++ demo.js
@@ -2,11 +2,12 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const Monk = require('monk');
+const ObjectID = require('mongodb').ObjectID;
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
 const router=new Router();
 const db=new Monk('localhost:27017/myproject');//链接到库
-const results = db.get('document');//表
+const blogCollection = db.get('document');//表
 const account = db.get('account');
 
 
@@ -24,13 +25,13 @@ router.get('/api', async ( ctx ) => {
 })
 
 router.get('/api/getList', async ( ctx ) => {
-  let data = await results.find();
+  let data = await blogCollection.find();
   ctx.response.type = 'application/json';
   ctx.body = data;
 })
 
 router.get('/api/getCategory', async ( ctx ) => {
-  let data = await results.distinct('category');
+  let data = await blogCollection.distinct('category');
   ctx.response.type = 'application/json';
   ctx.body = data;
 })
@@ -49,6 +50,118 @@ router.post('/api/validateUser', async ( ctx ) => {
   } else {
     ctx.body = {
       valid: false
+    }
+  }
+})
+
+router.post('/api/addBlog', async ( ctx ) => {
+  let blogName = ctx.request.body.blogName;
+  let link = ctx.request.body.link;
+  let category = ctx.request.body.category;
+
+  let findOne = await blogCollection.findOne({name: blogName});
+
+  if (findOne && findOne._id) {
+    ctx.body = {
+      success: false,
+      error: '添加博客失败(已有同名博客)'
+    }
+
+    return;
+  }
+
+  let result = await blogCollection.insert(
+    {
+      name: blogName,
+      link: link,
+      category: category
+    }
+  );
+
+  if (result._id) {
+    ctx.body = {
+      success: true
+    }
+  } else {
+    ctx.body = {
+      success: false,
+      error: '添加博客失败'
+    }
+  }
+})
+
+router.post('/api/deleteBlog', async ( ctx ) => {
+  let blogId = ctx.request.body.blogId;
+  let result;
+
+  try {
+    result = await blogCollection.findOneAndDelete(
+      {
+        _id: ObjectID(blogId)
+      }
+    );
+  } catch (e) {console.log(e.message)}
+
+  if (!result) {
+    ctx.body = {
+      success: false,
+      error: 'id不存在或不合法'
+    };
+  } else {
+    ctx.body = {
+      success: true
+    };
+  }
+})
+
+router.post('/api/updateBlog', async ( ctx ) => {
+  let blogId = ctx.request.body.blogId;
+  let blogName = ctx.request.body.blogName;
+  let link = ctx.request.body.link;
+  let category = ctx.request.body.category;
+
+  let result;
+
+  try {
+    result = await blogCollection.findOneAndUpdate(
+      {
+        _id: ObjectID(blogId),
+
+      },
+
+      {
+        name: blogName,
+        link: link,
+        category: category
+      }
+    );
+  } catch (e) {console.log(e.message)}
+
+  if (!result) {
+    ctx.body = {
+      success: false,
+      error: 'id不存在或不合法'
+    };
+  } else {
+    ctx.body = {
+      success: true
+    };
+  }
+})
+
+router.post('/api/findBlog', async ( ctx ) => {
+  let blogName = ctx.request.body.blogName;
+  let data = await blogCollection.findOne({name: blogName});
+
+  if (data) {
+    ctx.body = {
+      success: true,
+      result: data
+    }
+  } else {
+    ctx.body = {
+      success: false,
+      error: '该博客不存在'
     }
   }
 })
